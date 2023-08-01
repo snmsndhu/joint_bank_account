a08435af258221089f5c5a077b8a43684c3c7d2e
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-08-01 10:04:41 -0400

    smart contract

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -4,7 +4,22 @@ pragma solidity ^0.8.13;
 contract BankAccount {
 
     event Deposit (address indexed user, uint indexed accountId, uint value, uint timestamp);
-    event WithdrawRequest(address indexed user, uint indexed accountId, uint indexed withdrawId, uint amount, uint timestamp);
+    event WithdrawRequested(address indexed user, uint indexed accountId, uint indexed withdrawId, uint amount, uint timestamp);
+    event Withdraw(uint indexed withdrawId, uint timestamp);
+    event AccountCreated(address[] owners, uint indexed id, uint timestamp);
+
+    struct WithdrawRequest{
+        address user;
+        uint amount;
+        uint approvals;
+        mapping(address => bool) ownersApproved;
+        bool approved;
+    }
+    struct Account {
+        address[] owners;
+        uint balance;
+        mapping(uint => WithdrawRequest) withdrawRequests;
+    }
     constructor() {
         
     }
