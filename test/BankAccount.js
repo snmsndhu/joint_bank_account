3d76d0def0e47ed1857a0ee560b51512bfa81263
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-08-04 09:42:11 -0400

    testing smartcontract

--- test/BankAccount.js
+++ test/BankAccount.js
@@ -28,5 +28,53 @@ describe("BankAccount", function () {
       const accounts = await bankAccount.connect(addr0).getAccounts();
       expect(accounts.length).to.equal(1);
     });
+
+    it("should allow creating a double user account", async () => {
+      const { bankAccount, addr0, addr1 } = await loadFixture(
+        deployBankAccount
+      );
+      await bankAccount.connect(addr0).createAccount([addr1]);
+
+      const accounts1 = await bankAccount.connect(addr0).getAccounts();
+      expect(accounts1.length).to.equal(1);
+
+      const accounts2 = await bankAccount.connect(addr1).getAccounts();
+      expect(accounts2.length).to.equal(1);
+    });
+
+    it("should allow creating a triple user account", async () => {
+      const { bankAccount, addr0, addr1, addr2 } = await loadFixture(
+        deployBankAccount
+      );
+      await bankAccount.connect(addr0).createAccount([addr1, addr2]);
+
+      const accounts1 = await bankAccount.connect(addr0).getAccounts();
+      expect(accounts1.length).to.equal(1);
+
+      const accounts2 = await bankAccount.connect(addr1).getAccounts();
+      expect(accounts2.length).to.equal(1);
+
+      const accounts3 = await bankAccount.connect(addr2).getAccounts();
+      expect(accounts3.length).to.equal(1);
+    });
+
+    it("should allow creating a quad user account", async () => {
+      const { bankAccount, addr0, addr1, addr2, addr3 } = await loadFixture(
+        deployBankAccount
+      );
+      await bankAccount.connect(addr0).createAccount([addr1, addr2, addr3]);
+
+      const accounts1 = await bankAccount.connect(addr0).getAccounts();
+      expect(accounts1.length).to.equal(1);
+
+      const accounts2 = await bankAccount.connect(addr1).getAccounts();
+      expect(accounts2.length).to.equal(1);
+
+      const accounts3 = await bankAccount.connect(addr2).getAccounts();
+      expect(accounts3.length).to.equal(1);
+
+      const accounts4 = await bankAccount.connect(addr3).getAccounts();
+      expect(accounts4.length).to.equal(1);
+    });
   });
 });
