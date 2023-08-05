584194dacffc6a3df5959c871da26245c55a191a
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-08-05 18:13:09 -0400

    deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,20 +6,24 @@ async function main() {
   const bankAccount = await BankAccount.deploy();
 
   await bankAccount.deployed();
-
-  async function writeDeploymentInfo(contract) {
-    const data = {
-      contract: {
-        address: contract.address,
-        signerAddress: contract.signer.address,
-        abi: contract.interface.format(),
-      },
-    };
-    const content = JSON.stringify(data, null, 2);
-    await fs.writeFile("deployment.json", content, { encoding: "utf-8" });
-  }
+  await writeDeploymentInfo(bankAccount);
 }
 
+async function writeDeploymentInfo(contract) {
+  const data = {
+    contract: {
+      address: contract.address,
+      signerAddress: contract.signer.address,
+      abi: contract.interface.format(),
+    },
+  };
+
+  const content = JSON.stringify(data, null, 2);
+  await fs.writeFile("deployment.json", content, { encoding: "utf-8" });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
